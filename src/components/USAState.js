9f4909e633a44e651fe483c659290c62462bc56a
Adrian Fraiha <aef@fastmail.com>
2018-12-14 18:04:09 -0500

    add react-tooltip attrs

--- src/components/USAState.js
+++ src/components/USAState.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const USAState = (props) => {
   return (
-    <path d={props.dimensions} fill={props.fill} data-name={props.state} className={`${props.state} state`} onClick={props.onClickState} />
+    <path d={props.dimensions} fill={props.fill} data-name={props.state} className={`${props.state} state`} onClick={props.onClickState} data-tip data-for=${props.state}/>
   );
 }
 export default USAState;
